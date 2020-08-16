e4165d6b2e8b2195154e9522df28cd9c6b015c67
prawinkanna <45977611+prawinkanna@users.noreply.github.com>
2020-08-16 16:50:12 +0530

    initialised template

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+let skills = {
+	"html" : { "title" : "HTML", "known" : "80%", "bg_color" : "", "color" : "" },
+	"web_dev" : { "title" : "Web Development", "known" : "80%", "bg_color" : "", "color" : "" },
+	"front_end" : { "title" : "Frond-end Development", "known" : "80%", "bg_color" : "", "color" : "" },
+	"css" : { "title" : "CSS", "known" : "80%", "bg_color" : "", "color" : "" },
+	"responsive_web" : { "title" : "Responsive Web Design", "known" : "80%", "bg_color" : "", "color" : "" },
+	"javascript" : { "title" : "Javascript", "known" : "80%", "bg_color" : "", "color" : "" },
+	"jquery" : { "title" : "Jquery", "known" : "80%", "bg_color" : "", "color" : "" },
+	"json" : { "title" : "JSON", "known" : "80%", "bg_color" : "", "color" : "" },
+	"bootstrap" : { "title" : "Bootstrap", "known" : "80%", "bg_color" : "", "color" : "" },
+	"postman" : { "title" : "Postman API", "known" : "80%", "bg_color" : "", "color" : "" },
+	"onpage_opt" : { "title" : "On-Page Optimisation", "known" : "80%", "bg_color" : "", "color" : "" },
+	"onpage_seo" : { "title" : "On-Page SEO", "known" : "50%", "bg_color" : "", "color" : "" }
+};
+/*alert(skills.onpage_seo.known);*/
