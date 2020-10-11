6bf2d7dbbc2f3c4549d721bc392de958f2069a18
prawinkanna <45977611+prawinkanna@users.noreply.github.com>
2020-10-11 21:36:06 +0530

    uploaded news api v1

---
+++ projects/news-api-app/precache-manifest.0d528aeca6ff1f50c9ce18923e765cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57952f8e1cb5a2987819eb9ef53db080",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85a6974526aca7a65e5b",
+    "url": "./static/css/main.d4ab121b.chunk.css"
+  },
+  {
+    "revision": "ad4df57f68d7c0b42fba",
+    "url": "./static/js/2.1c5849c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1c5849c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a6974526aca7a65e5b",
+    "url": "./static/js/main.c46376de.chunk.js"
+  },
+  {
+    "revision": "e763245b6d3abd6e702d",
+    "url": "./static/js/runtime-main.c5c47813.js"
+  },
+  {
+    "revision": "506d53b87b3f8ccf82d924307a9aa4d3",
+    "url": "./static/media/banner1.506d53b8.jpg"
+  }
+]);
