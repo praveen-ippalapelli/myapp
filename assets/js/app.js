624cde70b80d14430d6b59acec6dd4bc19661bc5
prawinkanna <45977611+prawinkanna@users.noreply.github.com>
2020-10-15 21:24:18 +0530

    added news api

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,14 +4,16 @@ javascript = { "title" : "Javascript", "known" : "7", "bg_color" : "#f7e018", "c
 bootstrap = { "title" : "Bootstrap", "known" : "7", "bg_color" : "#533b78", "color" : "white" };
 jquery = { "title" : "Jquery", "known" : "6", "bg_color" : "#0865a6", "color" : "#f2f2f2" };
 mysql = { "title" : "MySql", "known" : "3", "bg_color" : "#00718b", "color" : "#ea8c10" };
-reactjs = { "title" : "React JS", "known" : "4", "bg_color" : "#222222", "color" : "#61dafb" };
-nodejs = { "title" : "Node JS", "known" : "3", "bg_color" : "#313131", "color" : "#64b648" };
-json = { "title" : "JSON", "known" : "4", "bg_color" : "#2f2f2f", "color" : "white" };
+reactjs = { "title" : "React", "known" : "4", "bg_color" : "#222222", "color" : "#61dafb" };
+nodejs = { "title" : "Node.js", "known" : "3", "bg_color" : "#313131", "color" : "#64b648" };
+json = { "title" : "JSON", "known" : "5", "bg_color" : "#2f2f2f", "color" : "white" };
 php = { "title" : "PHP", "known" : "3", "bg_color" : "#777bb3", "color" : "black" };
+mongodb = { "title" : "MongoDB", "known" : "3", "bg_color" : "#11924f", "color" : "white" };
+expressjs = { "title" : "Express.js", "known" : "3", "bg_color" : "#eee", "color" : "black" };
 
 
 
-var skill_array = [javascript, reactjs, mysql, nodejs, php, html, css, bootstrap, jquery, json];
+var skill_array = [javascript, reactjs, nodejs, mongodb, mysql, expressjs, php, html, css, bootstrap, jquery, json];
 
 	for(i=0; i<skill_array.length; i++){
 		$('.skills-list').append(
