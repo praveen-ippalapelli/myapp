93c8e0393d9858d48abb3c6f3bf9a73138df63be
prawinkanna <45977611+prawinkanna@users.noreply.github.com>
2020-11-24 13:08:14 +0530

    upddated

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,19 +7,23 @@ mysql = { "title" : "MySql", "known" : "3", "bg_color" : "#00718b", "color" : "#
 reactjs = { "title" : "React", "known" : "5", "bg_color" : "#222222", "color" : "#61dafb" };
 nodejs = { "title" : "Node.js", "known" : "3", "bg_color" : "#313131", "color" : "#64b648" };
 json = { "title" : "JSON", "known" : "6", "bg_color" : "#2f2f2f", "color" : "white" };
-php = { "title" : "PHP", "known" : "3", "bg_color" : "#777bb3", "color" : "black" };
+php = { "title" : "PHP", "known" : "4", "bg_color" : "#777bb3", "color" : "black" };
 mongodb = { "title" : "MongoDB", "known" : "3", "bg_color" : "#11924f", "color" : "white" };
 expressjs = { "title" : "Express.js", "known" : "3", "bg_color" : "#eee", "color" : "black" };
 kitsune = { "title" : "Kitsune Framework", "known" : "7", "bg_color" : "#f06428", "color" : "white" };
+redux = { "title" : "Redux", "known" : "5", "bg_color" : "#764abc", "color" : "white" };
 
 
 
-var skill_array = [javascript, reactjs, nodejs, mongodb, mysql, expressjs, kitsune, php, html, css, bootstrap, jquery, json];
-
+var skill_array = [javascript, reactjs, nodejs, mongodb, mysql, expressjs, kitsune, php, redux, html, css, bootstrap, jquery, json];
+	var gt_10 = '';
 	for(i=0; i<skill_array.length; i++){
+		if (i>10) {
+			gt_10 = 'gt-10'
+		}
 		$('.skills-list').append(
 		`
-		<div class="skill">
+		<div class="skill ${gt_10}">
 			<div class="level-bar" style="background:${skill_array[i].bg_color};width:${skill_array[i].known}0%;"></div>
 			<div class="skill-name" style="color:${skill_array[i].color};">${skill_array[i].title}</div>
 		</div>
