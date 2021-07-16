88986c7bf795eb67a9e64ebcba7f2b8fe8706ab9
praveen <45977611+prawinkanna@users.noreply.github.com>
2021-07-16 11:33:38 +0530

    updaed .net

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 html = { "title" : "HTML", "known" : "8", "bg_color" : "#e44d26", "color" : "white" };
-css = { "title" : "CSS", "known" : "8", "bg_color" : "#264de4", "color" : "white" };
+css = { "title" : "CSS", "known" : "7", "bg_color" : "#264de4", "color" : "white" };
 javascript = { "title" : "Javascript", "known" : "6", "bg_color" : "#f7e018", "color" : "black" };
 bootstrap = { "title" : "Bootstrap", "known" : "7", "bg_color" : "#533b78", "color" : "white" };
 jquery = { "title" : "Jquery", "known" : "6", "bg_color" : "#0865a6", "color" : "#f2f2f2" };
@@ -13,11 +13,13 @@ expressjs = { "title" : "Express.js", "known" : "4", "bg_color" : "#eee", "color
 kitsune = { "title" : "Kitsune Framework", "known" : "7", "bg_color" : "#f06428", "color" : "white" };
 redux = { "title" : "Redux", "known" : "4", "bg_color" : "#764abc", "color" : "white" };
 material_ui = { "title" : "Material UI", "known" : "4", "bg_color" : "#0081cb", "color" : "white" };
+c_sharp = { "title" : "C#", "known" : "2", "bg_color" : "#2b006e", "color" : "white" };
+asp_net = { "title" : "ASP.NET", "known" : "2", "bg_color" : "#cbeefc", "color" : "#155399" };
 
 
 
 var skill_array = [javascript, reactjs, nodejs, mongodb, mysql, expressjs, php, html, css,
- redux, material_ui, bootstrap, jquery, json];
+ c_sharp, asp_net, redux, material_ui, bootstrap, jquery, json];
 	var gt_10 = '';
 	for(i=0; i<skill_array.length; i++){
 		if (i>10) {
